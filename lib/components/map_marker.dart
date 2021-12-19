5e4d8f7b86ba92a31f2cbd92dcd479deb42e9ddc
duncanjake1 <jacobsenduncan@gmail.com>
2021-12-19 15:52:18 -0700

    began rework on toggle provider

--- lib/components/map_marker.dart
+++ lib/components/map_marker.dart
@@ -1,47 +1,33 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter/material.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:native_snocast/constants.dart';
-import 'package:native_snocast/controllers/map_marker_controller.dart';
 import 'package:native_snocast/main.dart';
 
+final currentFocusedMarkerProvider = StateProvider<Key?>((_) => null);
 
 // TODO: I think we may be able too turn this into a stateless widget, and just rebuild when provider updates
-class IndividualMarker extends ConsumerStatefulWidget {
+// we probably need a local provider for this. currentFocusedMarkerKeyProvider or something
+class IndividualMarker extends ConsumerWidget {
   final Key key;
   IndividualMarker({required this.key});
 
   @override
-  _IndividualMarkerState createState() => _IndividualMarkerState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    Key? currentFocusedMarkerKey = ref.watch(currentFocusedMarkerProvider);
+    bool isFocused = this.key == currentFocusedMarkerKey;
 
-class _IndividualMarkerState extends ConsumerState<IndividualMarker> {
-  bool isFocused = false;
+    print('marker says');
+    print(this.key);
+    print('provider says');
+    print(currentFocusedMarkerKey);
 
-  @override
-  Widget build(BuildContext context) {
-		final markerController = ref.watch(mapMarkerControllerProvider);
-    Key? currentFocusedMarkerKey = markerController.getCurrentFocusedMarkerKey;
-    if (widget.key == currentFocusedMarkerKey) {
-      setState(() {
-        isFocused = true;
-      });
-    } else {
-      setState(() {
-        isFocused = false;
-      });
-    }
     return GestureDetector(
       behavior: HitTestBehavior.translucent,
       onTap: () {
         // Set current Marker Key to this widgets key
-		  // also sets currentFocusedMarkerKey to null if widget.key and currentFocusedMarkerKey match
-        setState(() {
-              markerController.setCurrentFocusedMarkerKey = widget.key;
-							print(markerController.getCurrentFocusedMarkerKey);
-        });
+        // also sets currentFocusedMarkerKey to null if widget.key and currentFocusedMarkerKey match
+        currentFocusedMarkerKey = this.key;
       },
       child: Container(
         child: Stack(
